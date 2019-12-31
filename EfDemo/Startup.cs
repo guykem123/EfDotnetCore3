9b0d6a3f914b33d55e8b0145667264633f607b06
guyke <guyke@DESKTOP-94TA56C>
2019-12-31 16:08:41 +0200

    first commit

---
+++ EfDemo/Startup.cs
@@ -0,0 +1,50 @@
+using Cupcakes.Data;
+using Cupcakes.Repositories;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EfDemo
+{
+    public class Startup
+    {
+        private IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddTransient<ICupcakeRepository, CupcakeRepository>();
+
+            services.AddDbContext<CupcakeContext>(options =>
+                 options.UseSqlServer(_configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddControllers();
+            services.AddRazorPages();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, CupcakeContext ctx)
+        {
+            ctx.Database.EnsureCreated();
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "CupcakeRoute",
+                    pattern: "{controller=Cupcake}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
+            });
+        }
+    }
+}
