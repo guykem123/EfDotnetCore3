57f2984a1922fcb985ffb398ef86800ee5f0bb30
guyke <guyke@DESKTOP-94TA56C>
2020-01-01 09:40:51 +0200

    first commit

--- EfDemo/Startup.cs
+++ EfDemo/Startup.cs
@@ -32,8 +32,6 @@ namespace EfDemo
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, CupcakeContext ctx)
         {
-            ctx.Database.EnsureCreated();
-
             app.UseStaticFiles();
 
             app.UseRouting();
